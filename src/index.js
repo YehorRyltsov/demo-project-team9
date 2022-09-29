b3323e2cdc1fe56f5ede541d103349aa613da0a4
arseniy-grinev <arseniy.grinev@gmail.com>
2022-09-29 19:41:40 +0300

    fgfh

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import 'firebaseui/dist/firebaseui.css';
 
 import './js/user';
+
+import addWatchedFilmById from './js/db';
+
+const ref = {
+  addWatched: document.querySelector('#add-watched'),
+  addQueue: document.querySelector('#add-queue'),
+  removeWatched: document.querySelector('#remove-watched'),
+  removeQueue: document.querySelector('#remove-queue'),
+  getWatched: document.querySelector('#get-watched'),
+  getQueue: document.querySelector('#get-queue'),
+};
