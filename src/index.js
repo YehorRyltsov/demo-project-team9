d8293fa8c8dc3a9278681a1579b982e56d97018f
NataliiaKulchytska <kylchitskaia@gmail.com>
2022-09-29 22:00:42 +0300

    modal-team

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import 'firebaseui/dist/firebaseui.css';
 
 import './js/user';
+import './js/modal-team';
---
+++ src/js/footer.js
@@ -0,0 +1,205 @@
+import urlYegor from '../images/YegorRyltsov.jpg';
+import urlValerii from '../images/ValeriiPostrubailo.jpg';
+import urlKhrystyna from '..//images/KhrystynaMykolaichuk.jpg';
+import urlNataliiaK from '../images/Nataliia_Kulchytska.jpg';
+import urlNataliiaP from '../images/NataliyaPolyakova.jpg';
+import urlSergei from '../images/SergeiCherkesov.jpg';
+import urlVitaii from '../images/VitaiiMazur.jpg';
+import urlYuriy from '../images/YuriyShavurskiy.jpg';
+import urlDmytroKuzko from '../images/DmytroKuzko.jpg';
+import urlDmytroKoschavka from '../images/DmytroKoschavka.jpg';
+import urlArseny from '../images/ArsenyGrinev.jpg';
+import spriteUrl from '../images/icons.svg';
+
+ 
+export const markup = `<div class="modal-team" id="modalTeam"><div class="team-card">
+    <img src="${urlYegor}" alt="Yegor" class="team-image">
+    <p class="team-name">Yegor</p>
+    <p class="team-role">'Developer, TeamLead'</p>
+    <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#github"></use>
+    </svg></a>
+</div>
+<div class="team-card">
+    <img src="${urlValerii}" alt="Yegor" class="team-image">
+    <p class="team-name">Valerii</p>
+    <p class="team-role">'Developer, Scram master'</p>
+    <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#github"></use>
+    </svg></a>
+</div>
+<div class="team-card">
+    <img src="${urlKhrystyna}" alt="Yegor" class="team-image">
+    <p class="team-name">Khrystyna</p>
+    <p class="team-role">'Developer'</p>
+    <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#github"></use>
+    </svg></a>
+</div>
+<div class="team-card">
+    <img src="${urlNataliiaK}" alt="Yegor" class="team-image">
+    <p class="team-name">Nataliia</p>
+    <p class="team-role">'Developer'</p>
+    <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#github"></use>
+    </svg></a>
+</div>
+<div class="team-card">
+    <img src="${urlNataliiaP}" alt="Yegor" class="team-image">
+    <p class="team-name">Nataliia</p>
+    <p class="team-role">'Developer'</p>
+    <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#github"></use>
+    </svg></a>
+</div>
+<div class="team-card">
+    <img src="${urlSergei}" alt="Yegor" class="team-image">
+    <p class="team-name">Sergei</p>
+    <p class="team-role">'Developer'</p>
+    <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#github"></use>
+    </svg></a>
+</div>
+<div class="team-card">
+    <img src="${urlVitaii}" alt="Yegor" class="team-image">
+    <p class="team-name">Vitaii</p>
+    <p class="team-role">'Developer'</p>
+    <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#github"></use>
+    </svg></a>
+</div>
+<div class="team-card">
+    <img src="${urlYuriy}" alt="Yegor" class="team-image">
+    <p class="team-name">Yuriy</p>
+    <p class="team-role">'Developer'</p>
+    <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#github"></use>
+    </svg></a>
+</div>
+<div class="team-card">
+    <img src="${urlDmytroKoschavka}" alt="Yegor" class="team-image">
+    <p class="team-name">DmytroK</p>
+    <p class="team-role">'Developer'</p>
+    <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#github"></use>
+    </svg></a>
+</div>
+<div class="team-card">
+    <img src="${urlDmytroKuzko}" alt="Yegor" class="team-image">
+    <p class="team-name">Dmytro</p>
+    <p class="team-role">'Developer'</p>
+    <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#github"></use>
+    </svg></a>
+</div>
+<div class="team-card">
+    <img src="${urlArseny}" alt="Yegor" class="team-image">
+    <p class="team-name">Arseny</p>
+    <p class="team-role">'Developer'</p>
+    <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+      <use href="${spriteUrl}#github"></use>
+    </svg></a>
+</div>
+</div>
+`;
+
+
+// export const teamItems = [
+//   // {
+//   //   photo: '',
+//   //   name: 'Наша',
+//   //   family: 'команда',
+//   //   description: '',
+//   //   rol: '',
+//   //   gitHub: "https://github.com/",
+//   // },
+//   {
+//     photo: 'urlYegor',
+//     name: 'Yegor',
+//     family: 'Ryltsov',
+//     description: '',
+//     rol: 'Developer, TeamLead',
+//     gitHub: "https://github.com/YehorRyltsov",
+//     },
+//   {
+//     photo: 'urlValerii',
+//     name: 'Valerii',
+//     family: 'Postrubailo',
+//     description: '',
+//     rol: 'Developer, Scram master',
+//     gitHub: "https://github.com/Valerii-Postrybailo",
+//     },
+//    {
+//     photo: 'urlKhrystyna',
+//     name: 'Khrystyna',
+//     family: 'Mykolaichuk',
+//     description: '',
+//      rol: 'Developer',
+//     gitHub: "https://github.com/kmykolaichuk",
+//     },
+//       {
+//     photo: 'urlNataliiaK',
+//     name: 'Nataliia',
+//     family: 'Kulchytska',
+//     description: '',
+//         rol: 'Developer',
+//     gitHub: "https://github.com/NataliiaKulchytska",
+//     },
+//      {
+//     photo:' urlNataliiaP',
+//     name: 'Nataliia',
+//     family: 'Polyakova',
+//     description: '',
+//        rol: 'Developer',
+//     gitHub: "https://github.com/Nataliphoenix",
+//     },
+//     {
+//     photo: 'urlSergei',
+//     name: 'Sergei',
+//     family: 'Cherkesov',
+//     description: '',
+//       rol: 'Developer',
+//     gitHub: "https://github.com/seroga213",
+//     },
+//         {
+//     photo: 'urlVitaii',
+//     name: 'Vitaii',
+//     family: 'Mazur',
+//     description: '',
+//           rol: 'Developer',
+//     gitHub: "https://github.com/VitaliiMazur",
+//     },
+//                 {
+//     photo: 'urlYuriy',
+//     name: 'Yuriy',
+//     family: 'Shavurskiy',
+//     description: '',
+//                   rol: 'Developer',
+//     gitHub: "https://github.com/",
+//     },
+//                                {
+//     photo: 'urlArseny',
+//     name: 'Arseny',
+//     family: 'Grinev',
+//     description: '',
+//     rol: 'Developer',
+//     gitHub: "https://github.com/arseniy-grinev",
+//     },
+//                                                               {
+//     photo: 'urlDmytroKuzko',
+//     name: 'Dmitro',
+//     family: 'Kuzko',
+//     description: '',
+//     rol: 'Developer',
+//     gitHub: "https://github.com/",
+//     },
+//                                                                                              {
+//     photo: 'urlDmytroKoschavka',
+//     name: 'Dmytro',
+//     family: 'Koshchavka',
+//     description: '',
+//     rol: 'Developer',
+//     gitHub: "https://github.com/",
+//   },
+ 
+// ];
