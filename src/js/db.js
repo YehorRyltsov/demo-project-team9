f61bce3acb542615db81868034fe5f94ee9c5208
arseniy-grinev <arseniy.grinev@gmail.com>
2022-09-29 03:23:03 +0300

    add fns

---
+++ src/js/db.js
@@ -0,0 +1,11 @@
+export function addWatchedFilmById(userId, filmId) {}
+
+export function deleteWatchedFilmById(userId, filmId) {}
+
+export function addQueueFilmById(userId, filmId) {}
+
+export function deleteQueueFilmById(userId, filmId) {}
+
+export function getWatchedFilmsByUser(userId) {}
+
+export function getQueueFilmsByUser(userId) {}
