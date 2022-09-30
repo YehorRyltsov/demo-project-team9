a679ddeee0021b5378a4ca566e8fe777bed9aa00
NataliiaKulchytska <kylchitskaia@gmail.com>
2022-09-30 21:48:21 +0300

    github icon

--- src/js/footer.js
+++ src/js/footer.js
@@ -9,7 +9,7 @@ import urlYuriy from '../images/YuriyShavurskiy.jpg';
 import urlDmytroKuzko from '../images/DmytroKuzko.jpg';
 import urlDmytroKoschavka from '../images/DmytroKoschavka.jpg';
 import urlArseny from '../images/ArsenyGrinev.jpg';
-import spriteUrl from '../images/icons.svg';
+import spriteUrl from '../images/gitHub.svg';
 
  
 export const markup = `<div class="modal-team" id="modalTeam"><div class="team-card">
@@ -17,86 +17,86 @@ export const markup = `<div class="modal-team" id="modalTeam"><div class="team-c
     <p class="team-name">Yegor</p>
     <p class="team-role">'Developer, TeamLead'</p>
     <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
-      <use href="${spriteUrl}#github"></use>
+      <use  href="${spriteUrl}#github"></use>
     </svg></a>
 </div>
 <div class="team-card">
-    <img src="${urlValerii}" alt="Yegor" class="team-image">
+    <img src="${urlValerii}" alt="Valerii" class="team-image">
     <p class="team-name">Valerii</p>
     <p class="team-role">'Developer, Scram master'</p>
-    <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+    <a href="https://github.com/Valerii-Postrybailo" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
       <use href="${spriteUrl}#github"></use>
     </svg></a>
 </div>
 <div class="team-card">
-    <img src="${urlKhrystyna}" alt="Yegor" class="team-image">
+    <img src="${urlKhrystyna}" alt="Khrystyna" class="team-image">
     <p class="team-name">Khrystyna</p>
     <p class="team-role">'Developer'</p>
-    <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+    <a href="https://github.com/kmykolaichuk" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
       <use href="${spriteUrl}#github"></use>
     </svg></a>
 </div>
 <div class="team-card">
-    <img src="${urlNataliiaK}" alt="Yegor" class="team-image">
+    <img src="${urlNataliiaK}" alt="Nataliia" class="team-image">
     <p class="team-name">Nataliia</p>
     <p class="team-role">'Developer'</p>
-    <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+    <a href="https://github.com/NataliiaKulchytska" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
       <use href="${spriteUrl}#github"></use>
     </svg></a>
 </div>
 <div class="team-card">
-    <img src="${urlNataliiaP}" alt="Yegor" class="team-image">
+    <img src="${urlNataliiaP}" alt="Nataliia" class="team-image">
     <p class="team-name">Nataliia</p>
     <p class="team-role">'Developer'</p>
-    <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+    <a href="https://github.com/Nataliphoenix" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
       <use href="${spriteUrl}#github"></use>
     </svg></a>
 </div>
 <div class="team-card">
-    <img src="${urlSergei}" alt="Yegor" class="team-image">
+    <img src="${urlSergei}" alt="Sergei" class="team-image">
     <p class="team-name">Sergei</p>
     <p class="team-role">'Developer'</p>
-    <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+    <a href="https://github.com/seroga213" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
       <use href="${spriteUrl}#github"></use>
     </svg></a>
 </div>
 <div class="team-card">
-    <img src="${urlVitaii}" alt="Yegor" class="team-image">
+    <img src="${urlVitaii}" alt="Vitaii" class="team-image">
     <p class="team-name">Vitaii</p>
     <p class="team-role">'Developer'</p>
-    <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+    <a href="https://github.com/VitaliiMazur" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
       <use href="${spriteUrl}#github"></use>
     </svg></a>
 </div>
 <div class="team-card">
-    <img src="${urlYuriy}" alt="Yegor" class="team-image">
+    <img src="${urlYuriy}" alt=">Yuriy" class="team-image">
     <p class="team-name">Yuriy</p>
     <p class="team-role">'Developer'</p>
-    <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+    <a href="https://github.com/Shavursky" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
       <use href="${spriteUrl}#github"></use>
     </svg></a>
 </div>
 <div class="team-card">
-    <img src="${urlDmytroKoschavka}" alt="Yegor" class="team-image">
+    <img src="${urlDmytroKoschavka}" alt="Dmytro" class="team-image">
     <p class="team-name">DmytroK</p>
     <p class="team-role">'Developer'</p>
-    <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+    <a href="https://github.com/Scumsy" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
       <use href="${spriteUrl}#github"></use>
     </svg></a>
 </div>
 <div class="team-card">
-    <img src="${urlDmytroKuzko}" alt="Yegor" class="team-image">
+    <img src="${urlDmytroKuzko}" alt="Dmytro" class="team-image">
     <p class="team-name">Dmytro</p>
     <p class="team-role">'Developer'</p>
-    <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+    <a href="https://github.com/" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
       <use href="${spriteUrl}#github"></use>
     </svg></a>
 </div>
 <div class="team-card">
-    <img src="${urlArseny}" alt="Yegor" class="team-image">
+    <img src="${urlArseny}" alt="Arseny" class="team-image">
     <p class="team-name">Arseny</p>
     <p class="team-role">'Developer'</p>
-    <a href="https://github.com/YehorRyltsov" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
+    <a href="https://github.com/arseniy-grinev" target="_blank" class="team-git"><svg class="logo__icon" width="24" height="24">
       <use href="${spriteUrl}#github"></use>
     </svg></a>
 </div>
