00da000630dd8dc85f62042eca1cd933dddd280a
Dima Kuzko <bigmandarine1991@gmail.com>
2022-10-01 12:48:56 +0300

    test2

--- src/js/Local-Storage.js
+++ src/js/Local-Storage.js
@@ -10,19 +10,18 @@ import {
 } from './db';
 import { currentUserId } from './user';
 
-const KEY = 'a115fde3660c9e5b413d785f288ed44e';
-const btnWatchedEl = document.querySelector('#test-watched');
-const btnQueueEl = document.querySelector('#test-queue');
-async function fetchFilms() {
-  const data = await fetch(`
-     https://api.themoviedb.org/3/trending/all/week?api_key=${KEY}`);
-  const films = await data.json();
-  console.log(films.results);
-  return films.results;
-}
+const refs = {
+  KEY: 'a115fde3660c9e5b413d785f288ed44e',
+  btnWatchedEl: document.querySelector('#test-watched'),
+  btnQueueEl: document.querySelector('#test-queue'),
+  btnWatchedItemAdd: document.querySelector('.d-add-watched'),
+  btnWatchedItemRemove: document.querySelector('.d-remove-watched'),
+  btnQueueItemAdd: document.querySelector('.d-add-queue'),
+  btnQueueItemRemove: document.querySelector('.d-remove-queue'),
+};
 
-btnWatchedEl.addEventListener('click', () => onBtnClickWatched(760741));
-btnQueueEl.addEventListener('click', () => onBtnClickQueue(762504));
+refs.btnWatchedEl.addEventListener('click', () => onBtnClickWatched(438148));
+refs.btnQueueEl.addEventListener('click', () => onBtnClickQueue(762504));
 
 function onBtnClickQueue(filmId) {
   findFilmById(filmId).then(film => {
@@ -53,11 +52,28 @@ function onBtnClickWatched(filmId) {
 
 async function findFilmById(filmId) {
   const data = await fetch(`
-https://api.themoviedb.org/3/movie/${filmId}?api_key=${KEY}&language=en-US`);
+https://api.themoviedb.org/3/movie/${filmId}?api_key=${refs.KEY}&language=en-US`);
   const film = await data.json();
   //   console.log(film);
   return film;
 }
-findFilmById(113988);
-getWatchedFilmsByUser(currentUserId);
-getQueueFilmsByUser(currentUserId);
+
+async function onIsWatchedClick() {
+  const answer = await isWatched(currentUserId, 438148);
+  //   if (answer) {
+  //     refs.btnWatchedItemRemove.classList.add('show');
+  //     console.log(answer);
+  //   } else {
+  //     return;
+  //   }
+  console.log(answer);
+}
+onIsWatchedClick();
+async function onIsQueueClick() {
+  const answer = await isQueue(currentUserId, folm.id);
+  if (answer) {
+    refs.btnQueueItemRemove.classList.add('show');
+  } else {
+    return;
+  }
+}
