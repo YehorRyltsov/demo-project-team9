312e2de8fbe0191d63031f2a4d08700bf2f94d65
Dima Kuzko <bigmandarine1991@gmail.com>
2022-10-01 19:55:10 +0300

    fixed

--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -1,23 +1,27 @@
 import * as basicLightbox from 'basiclightbox';
 import '../../node_modules/basiclightbox/dist/basicLightbox.min.css';
 
-import { markup } from './footer';
+import  {markup } from './footer';
 
-const openModal = document.querySelector('[data-modal-open-team]');
 
+  const openModal = document.querySelector('[data-modal-open-team]')
+ 
 // console.log("1", openModal);
 
+
 openModal.addEventListener('click', onOpenModal);
-const modal = basicLightbox.create(markup);
+ const modal = basicLightbox.create(markup);
 
 function onOpenModal() {
   modal.show();
   window.addEventListener('keydown', onCloseModal);
+
+ 
 }
-function onCloseModal(e) {
-  if (e.code === 'Escape') {
-    modal.close();
-    window.removeEventListener('keydown', onCloseModal);
+  function onCloseModal(e) {
+    if (e.code === 'Escape') {
+      modal.close();
+      window.removeEventListener('keydown', onCloseModal);
+    }
+    openModal.removeEventListener('click', onOpenModal);
   }
-  openModal.removeEventListener('click', onOpenModal);
-}
