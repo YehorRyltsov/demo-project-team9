92768cff36ddb5927cd4564599ba2f4aa6fbd612
Khrystyna Mykolaichuk <xrustyaxl@ukr.net>
2022-10-01 18:19:30 +0300

    Изменяет main-movies на то, что было вчера

--- src/js/main-movies.js
+++ src/js/main-movies.js
@@ -1,35 +1,50 @@
-import { fetchMoves, fetchGenres } from './fetch-movies.js';
+const cardList = document.querySelector('.gallery-films');
 
-const div = document.querySelector('.gallary-films');
+export function fetchMoves() {
+  fetch(
+    'https://api.themoviedb.org/3/trending/movie/week?api_key=cfb01d01cf730c010b0d096c15822b40'
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(movies => {
+      fetch(
+        'https://api.themoviedb.org/3//genre/movie/list?api_key=cfb01d01cf730c010b0d096c15822b40'
+      )
+        .then(response => {
+          return response.json();
+        })
+        .then(genres => {
+          // console.log(genres);
+          const arrayGen = genres.genres;
+          console.log(arrayGen);
 
-fetchMoves().then(movies => {
-  fetchGenres().then(genres => {
-    const arrayGen = genres.genres;
-    console.log(arrayGen);
+          const filmsArr = movies.results;
+          console.log(filmsArr);
 
-    const filmsArr = movies.results;
-    console.log(filmsArr);
-
-    const arrMove = filmsArr
-      .map(move => {
-        const genres = move.genre_ids.map(id => {
-          for (let genre of arrayGen) {
-            if (id === genre.id) {
-              return genre.name;
-            }
-          }
-        });
-        move.genres = genres;
-        return move;
-      })
-      .map(move => {
-        const date = new Date(`${move.release_date}`);
-        const year = date.getFullYear();
-        return `
-                <div class="photo-card">
+          const arrMove = filmsArr
+            .map(move => {
+              const genres = move.genre_ids.map(id => {
+                for (let genre of arrayGen) {
+                  if (id === genre.id) {
+                    return genre.name;
+                  }
+                }
+              });
+              move.genres = genres;
+              return move;
+            })
+            .map(move => {
+              const date = new Date(`${move.release_date}`);
+              const year = date.getFullYear();
+              return `
+                <li class="photo-card">
+                <a class="link" href="#">
                   <img src= "https://image.tmdb.org/t/p/w500${
                     move.poster_path
-                  }" alt="${move.original_title} loading="lazy" width: 0px>
+                  }" alt="${
+                move.original_title
+              } loading="lazy" width: 0px class="card-image">
                   <div class="info">
                     <p class="info-item">
                       <b>${move.title.toUpperCase()}</b>
@@ -39,11 +54,13 @@ fetchMoves().then(movies => {
                       <b class="info-genres"> | </b>
                       <b class="info-genres">${year}</b>
                     </p>                   
-                  </a>
                   </div>
-                </div>`;
-      })
-      .join('');
-    div.insertAdjacentHTML('afterbegin', arrMove);
-  });
-});
+                  </a>
+                </li>`;
+            })
+            .join('');
+          cardList.insertAdjacentHTML('afterbegin', arrMove);
+        });
+    });
+}
+fetchMoves();
