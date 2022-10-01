6b62bbfe0f4eaf285e90243c4457fba02b9d4c0c
Shavursky <shavursky@gmail.com>
2022-10-01 20:18:45 +0300

    Update main-movies.js

--- src/js/main-movies.js
+++ src/js/main-movies.js
@@ -1,19 +1,9 @@
+import {fetchMoves, fetchGenres} from './fetch-movies';
 const cardList = document.querySelector('.gallery-films');
 
-export function fetchMoves() {
-  fetch(
-    'https://api.themoviedb.org/3/trending/movie/week?api_key=cfb01d01cf730c010b0d096c15822b40'
-  )
-    .then(response => {
-      return response.json();
-    })
+fetchMoves()
     .then(movies => {
-      fetch(
-        'https://api.themoviedb.org/3//genre/movie/list?api_key=cfb01d01cf730c010b0d096c15822b40'
-      )
-        .then(response => {
-          return response.json();
-        })
+      fetchGenres ()
         .then(genres => {
           // console.log(genres);
           const arrayGen = genres.genres;
@@ -62,5 +52,4 @@ export function fetchMoves() {
           cardList.insertAdjacentHTML('afterbegin', arrMove);
         });
     });
-}
-fetchMoves();
+
