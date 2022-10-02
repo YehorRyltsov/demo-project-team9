24c695873a4888097a4bb1ccca449f640c8f1f5d
Natali Poliakova <natalipoliakova7@gmail.com>
2022-10-02 15:56:27 +0200

    review

--- src/js/modal-review.js
+++ src/js/modal-review.js
@@ -1,27 +1,29 @@
-// import {onCardClick} from './main-movies'; 
+
+
+
+
+
 
 const refs = {
-    
     modal: document.querySelector("[data-modal]"),
     body:document.querySelector ("body"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
+    
 
-    // modalImgReview:document.querySelector('#img-review'),
-    // captionImgText:document.querySelector('#caption-review'),
-    // voteImgReview:document.querySelector('#vote-review'),
-    // votesImgReview:document.querySelector('#votes-review'),
-    // popularityImgReview:document.querySelector('#popularity-review'),
-    // titleImgReview:document.querySelector('#title-review'),
-    // genreImgReview:document.querySelector('#genre-review'),
-    // aboutImgReview:document.querySelector('#about-review'),
+
+
+
+
+
+
+
+
+
+
   };
-  // import {onCardClick} from './modal-review'; 
-  // data-idcard="${move.id}
-  // cardList.addEventListener('click', onCardClick);
 
-  refs.closeModalBtn.addEventListener("click", closeModalReview);
 
-  export function openModalReview() {
+  function openModalReview() {
+    
     refs.modal.classList.remove("is-hidden");
     refs.body.classList.add("no-scroll");
 
@@ -29,6 +31,7 @@ const refs = {
     window.addEventListener('keydown', onEscKeyPressExit);
 
   }
+
 
   export function onCardClick(e){
     e.preventDefault();
@@ -41,8 +44,9 @@ const refs = {
    
   }
 
+
   async function fetchMoveId(сardId){
-    return fetch(`https://api.themoviedb.org/3/movie/${сardId}?api_key=cfb01d01cf730c010b0d096c15822b40`)
+    return await fetch(`https://api.themoviedb.org/3/movie/${сardId}?api_key=cfb01d01cf730c010b0d096c15822b40`)
     .then(response => {
       if (!response.ok) {
         throw new Error(response.status);
@@ -53,39 +57,48 @@ const refs = {
 }
 
 
-function renderModalCardReview(card){
-  // refs.modalImgReview.src = card.belongs_to_collection.poster_path? card.belongs_to_collection.poster_path : './images/imgModalReview.jpg';
-  // refs.captionImgText.textContent = card.title;
-
-  // refs.voteImgReview.textContent = card.vote_average.toFixed(1);
-  // refs.votesImgReview.textContent = card.vote_count.toFixed(0);
-  // refs.popularityImgReview.textContent = card.popularity.toFixed(1);
-  // refs.titleImgReview.textContent = card.original_title;
-  // refs.genreImgReview.textContent = card.genres.map(genre => genre.name).join(", ");
-  // refs.aboutImgReview.textContent = card.overview? card.overview : 'An excellent film and worthy of your attention!';
-
+function closeModalReview(){
+  refs.modal.classList.add("is-hidden");
+  refs.body.classList.remove("no-scroll");
   
+  refs.modal.removeEventListener('click', onBackdropClick);
+  window.removeEventListener('keydown', onEscKeyPressExit);
+  
+  }
   
 
+  function onBackdropClick(event) {
+    if (event.target === event.currentTarget) {
+      closeModalReview();
+
+    }
+  }
+
   
-  refs.modal.innerHTML = card.map( move => {
-    return `<div class="modal-review">
+  function onEscKeyPressExit(event) {
+    if (event.code === 'Escape') {
+      closeModalReview();
+
+    }
+  }
+
+
+  function renderModalCardReview(move){
+  refs.modal.innerHTML = `<div class="modal-review">
     <button
-      type="button"
-      class="review__btn-close"
-      data-modal-close
-      aria-label="Close"
+    type ="button" class="review__btn-close close"
     >
-      <svg width="30" height="30">
-        <use href="./images/icons.svg#close"></use>
-      </svg>
     </button>
     <div class="modal-review__body">
       <div class="modal-review__card">
         <img class="modal-review__content" 
-        src="https://image.tmdb.org/t/p/w500${move.belongs_to_collection.poster_path? card.belongs_to_collection.poster_path : './images/imgModalReview.jpg'}" />
+        src="https://image.tmdb.org/t/p/w500${
+            move.poster_path}"
+            ? "https://image.tmdb.org/t/p/w500${
+            move.poster_path}"
+            : "./images/imgModalReview.jpg" />
       </div>
-      <div>
+      <div class="modal-review__container">
         <h2 class="modal-review__title" id="caption-review">${move.title}</h2>
         <ul class="modal-review__items">
           <li class="modal-review__item list">Vote / Votes</li>
@@ -104,67 +117,18 @@ function renderModalCardReview(card){
           <p class="modal-review__item--dark" id="genre-review">${move.genres.map(genre => genre.name).join(", ")} </p>
         </ul>
         <p class="modal-review__subtitle">About</p>
-        <p class="modal-review__about" id="about-review">${move.overview? card.overview : 'An excellent film and worthy of your attention!'}
+        <p class="modal-review__about" id="about-review">${move.overview? move.overview : 'An excellent film and worthy of your attention!'}
         </p>
         <div class="modal-review__btn-container">
           <button type="button" class="modal-review__btn">
-            add to Watched
+            ADD TO WACHAD
           </button>
-          <button type="button" class="modal-review__btn">add to queue</button>
+          <button type="button" class="modal-review__btn">ADD TO QUEUE</button>
         </div>
       </div>
     </div>
   </div>`;
-})
-.join('');
 
-
-
+  const closeModalBtn = document.querySelector(".review__btn-close");
+  closeModalBtn.addEventListener("click", closeModalReview);
 }
- 
-
-
-
-
-
-   function closeModalReview(){
-    refs.modal.classList.add("is-hidden");
-    refs.body.classList.remove("no-scroll");
-
-    refs.modal.removeEventListener('click', onBackdropClick);
-    window.removeEventListener('keydown', onEscKeyPressExit);
-
-  
-  }
-
-function onBackdropClick(event) {
-  if (event.target === event.currentTarget) {
-    closeModalReview();
-  }
-}
-
-function onEscKeyPressExit(event) {
-  if (event.code === 'Escape') {
-    closeModalReview();
-  }
-}
-
-
-
-
-  // const {
-  //   id,
-  //   backdrop_path = './images/imgModalReview.jpg',
-  //   title,
-  //   vote_average = '10',
-  //   vote_count = '10',
-  //   popylarity,
-  //   original_title,
-  //   genre_ids,
-  //   overview = 'An excellent film and worthy of your attention!',
-
-  // } = returnObj;
-
-  // refs.modalImgReview.src = backdrop_path;
-  // refs.captionImgText.textContentL = title;
-  // refs.aboutImgReview.textContent = overview;
