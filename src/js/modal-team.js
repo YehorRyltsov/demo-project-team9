ca377e42898ee08a9d08f88d767bc6a77204afb6
NataliiaKulchytska <kylchitskaia@gmail.com>
2022-10-02 22:47:00 +0300

    function
    Закомментировала

--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -6,34 +6,41 @@ import  {markup } from './footer';
 
 const openModal = document.querySelector('[data-modal-open-team]');
 const body = document.querySelector('body');
-//  const backdrop = document.querySelector('.modal-team');
-
-
+ const backdrop = document.querySelector('#modalTeam');
+const lightBox = document.querySelector('.basicLightbox--visible');
 
 openModal.addEventListener('click', onOpenModal);
  const modal = basicLightbox.create(markup);
 
+window.addEventListener('keydown', onCloseModal);
+
 function onOpenModal(e) {
   e.preventDefault();
   modal.show();
-  window.addEventListener('keydown', onCloseModal);
+  
+  // body.classList.add('noScroll');
+  // lightBox.addEventListener('click', onClickBackdrop); 
+  
   onCloseModal();
- 
-}
-function onCloseModal(e) {
-    
-    if (e.code === 'Escape') {
+  onClickBackdrop();
+  openModal.removeEventListener('click', onOpenModal);
+};
+  
+function onCloseModal(event) {    
+   
+    if (event.code === 'Escape') {
       modal.close();
-      //  body.classList.remove('noScroll');
+      // body.classList.remove('noScroll');
       window.removeEventListener('keydown', onCloseModal);
     }
-    // openModal.removeEventListener('click', onOpenModal);
-  }
-// function clickBackdrop(e) {
-//   if (!e.currentTarget === backdrop) {
-//   modal.close();
-//        body.classList.remove('noScroll');
-//     openModal.removeEventListener('click', clickBackdrop);
-//    
-//   }
+   
+};
+// function onClickBackdrop(event) {  
+//   if (!backdrop === event.currentTarget) {
+//    modal.close(); 
+//     lightBox.removeEventListener('click', onClickBackdrop);
+//      body.classList.remove('noScroll');
+     
+//   }    
 // };
+// console.log('111', lightBox);
