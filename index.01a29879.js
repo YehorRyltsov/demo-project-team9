9b929e77d8c22030f1d7a6f5093e4db6ca32fe8f
YehorRyltsov <99420905+YehorRyltsov@users.noreply.github.com>
2022-10-03 08:04:21 +0000

    Deploying to gh-pages from @ YehorRyltsov/demo-project-team9@e54e1da95d8f5195d3ea6ecc03bc65227bb7797c 🚀

--- index.01a29879.js
+++
@@ -1,2 +0,0 @@
-window.addEventListener("load",(()=>{setTimeout((()=>{loaderContainer.classList.contains("loader-is-open")||loaderContainer.classList.add("loader-is-open")}),600)}));
-//# sourceMappingURL=index.01a29879.js.map
--- index.4cdc43ef.js
+++
@@ -1,2 +0,0 @@
-window.addEventListener("load",(function(){setTimeout((function(){loaderContainer.classList.contains("loader-is-open")||loaderContainer.classList.add("loader-is-open")}),600)}));
-//# sourceMappingURL=index.4cdc43ef.js.map
