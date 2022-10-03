851b3ad40791bc4bfad44d24a40e8299084c4370
Scumsy <dmitry.koschavka@gmail.com>
2022-10-03 16:10:04 +0300

    Update my-library-header.js

--- src/js/my-library-header.js
+++ src/js/my-library-header.js
@@ -1,18 +1,11 @@
 import { getWatchedFilmsByUser, getQueueFilmsByUser } from './db';
 import { currentUserId } from './user';
 import { showModal } from './hideAndOpen-modal';
-import { fetchMoves, fetchGenres } from './fetch-movies';
-
 import { createPagination } from './pagination';
+import emptyLibraryPageUrl from '../images/empty-library-page.jpg';
 
-// console.log(currentUserId);
-
-// let movieWatchedList = getWatchedFilmsByUser(currentUserId);
-// let movieQueueList = getQueueFilmsByUser(currentUserId);
 let movieWatchedList = [];
 let movieQueueList = [];
-
-import emptyLibraryPageUrl from '../images/empty-library-page.jpg';
 
 const header = document.querySelector('.header');
 const searchInput = document.querySelector('.search-form');
@@ -24,6 +17,7 @@ const queueBtn = document.querySelector('.queue-btn');
 const logo = document.querySelector('.logo-link');
 const sectionPagination = document.querySelector('.section-pagination');
 const cardList = document.querySelector('.gallery-films');
+const pagin = document.querySelector('#pagination');
 
 homeHeader.addEventListener('click', onHeaderHomeClick);
 myLibraryHeader.addEventListener('click', onMyLibraryClick);
@@ -40,13 +34,13 @@ export function onHeaderHomeClick(evt) {
   header.classList.add('home-header-bg');
   pagin.style.display = 'flex';
   cardList.classList.add('gallery-films');
-  // onClickReturnFromLibrary();
 }
 
 function onMyLibraryClick(evt) {
   evt.preventDefault();
-  getDataByUserID();
-  console.log(movieQueueList);
+   watchedBtn.classList.remove('active');
+  queueBtn.classList.add('active');
+  getQueueMoviesByUserID();
   if (currentUserId !== null) {
     searchInput.classList.add('visually-hidden');
     myLibraryBtnWrap.classList.remove('visually-hidden');
@@ -59,8 +53,11 @@ function onMyLibraryClick(evt) {
     showModal();
   }
 }
-function getDataByUserID() {
-  return console.log((movieQueueList = getQueueFilmsByUser(currentUserId)));
+function getQueueMoviesByUserID() {
+  return (movieQueueList = getQueueFilmsByUser(currentUserId));
+}
+function getWatchedMoviesByUserID() {
+  return (movieWatchedList = getWatchedFilmsByUser(currentUserId));
 }
 
 function onWatchedBtnClick() {
@@ -68,7 +65,7 @@ function onWatchedBtnClick() {
   watchedBtn.classList.add('active');
   watchedBtn.disabled = true;
   queueBtn.disabled = false;
-  getDataByUserID;
+  getWatchedMoviesByUserID();
   retrieveWatchedMovies(movieWatchedList);
 }
 
@@ -77,7 +74,7 @@ function onQueueBtnClick() {
   queueBtn.classList.add('active');
   watchedBtn.disabled = false;
   queueBtn.disabled = true;
-  getDataByUserID();
+  getQueueMoviesByUserID();
   retrieveWatchedMovies(movieQueueList);
 }
 
@@ -87,8 +84,6 @@ export { onWatchedBtnClick, onQueueBtnClick };
 
 function showEmptyLibrary() {
   cardList.style.justifyContent = 'center';
-  // cardList.style.margin = '0';
-
   cardList.innerHTML = `<li>
   <p class="empty-library__text"> There is nothing here yet! </p>
   <img class="empty-library__image" src="${emptyLibraryPageUrl}" alt="empty cinema hall">
@@ -104,7 +99,6 @@ function retrieveWatchedMovies(movies) {
     if (movies.length !== 0) {
       return renderWatchedList(movies);
     } else {
-      console.log('No Movies');
       showEmptyLibrary();
       sectionPagination.style.display = 'none';
     }
@@ -124,17 +118,22 @@ function renderWatchedList(array) {
     );
   });
 
-  // cardList.innerHTML = renderMovies;
-  cardList.innerHTML = '';
+    cardList.innerHTML = '';
   cardList.insertAdjacentHTML('afterbegin', renderMovies.join(''));
-  // createPagination(page, 20, array.total_results);
-  if (array.lenght > 20) {
-    createPagination(page, 20, array.length);
-  }
 
-  // cardList.insertAdjacentHTML('beforeend', renderMovies); //---- указать контейнер в котором рендерить список
-}
-
+  //------- Настроить ПАГИНАЦИЮ ----------------------!!!!!!!!!!!
+//   if (array.length > 10) {
+//     console.log('PAGINATION SHOUL BE HERE');
+//     // libraryPagination(2, array);
+//     createPagination(2, 10, array.length);
+//   } else {
+//     sectionPagination.style.display = 'none';
+//   }
+// }
+// function libraryPagination(page, array) {
+//   createPagination(page, 10, array.length);
+// }
+// ------------------------------------------------------------!!!!!!!!!!
 function makeMoviesListMarkup(
   poster_path,
   original_title,
@@ -168,58 +167,3 @@ function makeMoviesListMarkup(
                 </li>`;
   return markup;
 }
-
-// const pagin = document.querySelector('#pagination');
-// function onClickReturnFromLibrary() {
-//   fetchMoves().then(movies => {
-//     fetchGenres().then(genres => {
-//       const arrayGen = genres.genres;
-
-//       const filmsArr = movies.results;
-//       console.log(filmsArr);
-
-//       const arrMove = filmsArr
-//         .map(move => {
-//           const genres = move.genre_ids.map(id => {
-//             for (let genre of arrayGen) {
-//               if (id === genre.id) {
-//                 return genre.name;
-//               }
-//             }
-//           });
-//           move.genres = genres;
-//           return move;
-//         })
-//         .map(move => {
-//           const date = new Date(`${move.release_date}`);
-//           const year = date.getFullYear();
-//           return `
-//                 <li class="photo-card">
-//                 <a class="link" href="#">
-//                   <img src= "https://image.tmdb.org/t/p/w500${
-//                     move.poster_path
-//                   }" alt="${
-//             move.original_title
-//           } loading="lazy" width: 0px class="card-image">
-//                   <div class="info">
-//                     <p class="info-item">
-//                       <b>${move.title.toUpperCase()}</b>
-//                     </p>
-//                     <p class="info-item">
-//                       <b class="info-genres">${move.genres.join(', ')}</b>
-//                       <b class="info-genres"> | </b>
-//                       <b class="info-genres">${year}</b>
-//                     </p>
-//                   </div>
-//                   </a>
-//                 </li>`;
-//         })
-//         .join('');
-//       // cardList.insertAdjacentHTML('afterbegin', arrMove);
-//       cardList.innerHTML = arrMove;
-
-//       // createPagination(page, 20, arrMove.total_results);
-//       // console.log(createPagination(1, 20, movies.total_results));
-//     });
-//   });
-// }
