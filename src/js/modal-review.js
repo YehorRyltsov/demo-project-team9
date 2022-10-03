b6d25b0a3ff26671f53d5b7ccfaaa1e8caf23b01
Dima Kuzko <bigmandarine1991@gmail.com>
2022-10-03 18:06:02 +0300

    error

--- src/js/modal-review.js
+++ src/js/modal-review.js
@@ -1,7 +1,5 @@
-import { currentUserId } from './user';
-import { showModal } from './hideAndOpen-modal';
-// import { isWatched } from './db';
-import { onBtnClickWatched } from './push-data-to-database';
+import { hideLoader, showLoader } from './pre_loader';
+
 const refs = {
   modal: document.querySelector('[data-modal]'),
   body: document.querySelector('body'),
@@ -24,6 +22,7 @@ function openModalReview() {
 
 export function onCardClick(e) {
   e.preventDefault();
+  showLoader();
   const сardId = e.target.closest('li').dataset.idcard;
   openModalReview();
   fetchMoveId(сardId)
@@ -120,63 +119,10 @@ function renderModalCardReview(move) {
     </div>
   </div>`;
 
-  // const closeModalBtn = document.querySelector('.review__btn-close');
-  // closeModalBtn.addEventListener('click', closeModalReview);
-  // // --------------------------------------------  DIMA  ------------------------------------------------
-  // // isCurrentUserLog(currentUserId);
-  // const btnWatchedEl = document.querySelector('.watched');
-  // const btnQueueEl = document.querySelector('.queue');
-  // const btnWatchedItemAdd = document.querySelector('.d-add-watched');
-  // const btnWatchedItemRemove = document.querySelector('.d-remove-watched');
-  // const btnQueueItemAdd = document.querySelector('.d-add-queue');
-  // const btnQueueItemRemove = document.querySelector('.d-remove-queue');
+  hideLoader();
 
-  // answerIsWatched(currentUserId, move.id).then(answer => {
-  //   if (answer) {
-  //     btnWatchedItemAdd.classList.add('hide');
-  //     btnWatchedItemRemove.classList.remove('hide');
-
-  //     console.log(answer);
-  //   } else {
-  //     btnWatchedItemAdd.classList.remove('hide');
-  //     btnWatchedItemRemove.classList.add('hide');
-  //   }
-  // });
-  // answerIsQueue(currentUserId, move.id).then(answer => {
-  //   if (answer) {
-  //     btnQueueItemAdd.classList.add('hide');
-  //     btnQueueItemRemove.classList.remove('hide');
-
-  //     console.log(answer);
-  //   } else {
-  //     btnQueueItemAdd.classList.remove('hide');
-  //     btnQueueItemRemove.classList.add('hide');
-  //   }
-  // });
-  // btnQueueEl.classList.remove('active');
-  // btnWatchedEl.addEventListener('click', () => onBtnClickWatched(move.id));
-  // btnQueueEl.addEventListener('click', () => onBtnClickQueue(move.id));
-  // btnWatchedEl.addEventListener('click', () => TestOnBtnClick(currentUserId));
-  // -------------- При клике на кнопку Watched ---------------
-
-  // async function onBtnClickWatched(filmId) {
-  // btnWatchedEl.classList.add('active');
-  // btnQueueEl.classList.remove('active');
-  // const answer = await isWatched(currentUserId, filmId);
-
-  // findFilmById(filmId).then(film => {
-  //   if (answer) {
-  //     deleteWatchedFilmById(currentUserId, filmId);
-  //     console.log('вызвали удалить');
-  //     return;
-  //   } else {
-  //     addWatchedFilmByUser(currentUserId, film);
-  //     console.log('вызвали добавить');
-  //     console.log(film);
-  //   }
-  // });
-  // btnWatchedItemAdd.classList.toggle('hide');
-  // btnWatchedItemRemove.classList.toggle('hide');
+  const closeModalBtn = document.querySelector('.review__btn-close');
+  closeModalBtn.addEventListener('click', closeModalReview);
 }
 // -------------- При клике на кнопку Queue ---------------
 // async function onBtnClickQueue(filmId) {
